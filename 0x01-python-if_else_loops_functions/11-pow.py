941fcd4a09e3ef14f23ca7eedd5c34730f2f04c8
SMMaina <mainamuthoni4814@gmail.com>
2022-06-28 05:48:00 -0700

    twelfth

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 != 0:
-        print("{:c}".format(i - 32), end="")
-    else:
-        print("{:c}".format(i), end="")
+def pow(a, b):
+    return (a**b)
