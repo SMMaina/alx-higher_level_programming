083e295db8b8a2af7eefc12ee85481445d87d673
SMMaina <mainamuthoni4814@gmail.com>
2022-06-30 02:39:11 -0700

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name == "__main__":
+    argno = len(sys.argv) - 1
+    colon = ':.'
+    if argno > 0:
+        if argno == 1:
+            print("{:d} argument:".format(argno))
+        else:
+            print("{:d} arguments{:s}".format(argno, colon[0]))
+        for i in range(1, argno + 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+    if argno == 0:
+        print("{:d} arguments{:s}".format(argno, colon[1]))
