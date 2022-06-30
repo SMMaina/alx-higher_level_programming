a29550530be29b8e1dc64c723724024859418399
SMMaina <mainamuthoni4814@gmail.com>
2022-06-30 02:48:35 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     argno = len(sys.argv)
     add = 0
     if argno > 1:
-        for i in range(1,argno):
+        for i in range(1, argno):
             add+=int(sys.argv[i])
     print(add)
