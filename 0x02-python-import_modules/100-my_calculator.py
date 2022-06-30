ebb9c57c26d1780a1ecf27659b026ef5849f5d2f
SMMaina <mainamuthoni4814@gmail.com>
2022-06-30 03:26:00 -0700

    more calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,26 +2,26 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    sign = '+-*/'
+    operator = '+-*/'
     argno = len(sys.argv) - 1
-    error = ["Usage: ./100-my_calculator.py <a> <operator> <b>",
+    errormsg = ["Usage: ./100-my_calculator.py <a> <operator> <b>",
              "Unknown operator. Available operators: +, -, * and /"]
     if argno == 3:
-        if sys.argv[2] in sign:
+        if sys.argv[2] in operator:
             a = int(sys.argv[1])
             b = int(sys.argv[3])
-            if sys.argv[2] == sign[0]:
+            if sys.argv[2] == operator[0]:
                 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-            if sys.argv[2] == sign[1]:
+            if sys.argv[2] == operator[1]:
                 print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-            if sys.argv[2] == sign[2]:
+            if sys.argv[2] == operator[2]:
                 print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-            if sys.argv[2] == sign[3]:
+            if sys.argv[2] == operator[3]:
                 print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
-            print("{}".format{error[1]))
+            print("{}".format{errormsg[1]))
             exit(1)
     else:
-        print("{}".format(error[0]))
+        print("{}".format(errormsg[0]))
         exit(1)
 
