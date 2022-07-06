49370c9bb380fd1282f2ade7d180785d775a57e0
SMMaina <mainamuthoni4814@gmail.com>
2022-07-06 04:26:38 -0700

    weighted

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        sumd = sump = multiaxb = 0
+        for tuple in my_list:
+            sumd += tuple[1]
+            multiaxb = tuple[0] * tuple[1]
+            sump = multiaxb
+        return (sump / sumd)
+    return 0
