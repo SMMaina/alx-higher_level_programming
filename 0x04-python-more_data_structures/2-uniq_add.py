6345b2910ce3904fbac0d7301ef6781248b59d3e
SMMaina <mainamuthoni4814@gmail.com>
2022-07-06 03:17:13 -0700

    add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if my_list:
+        addlist = set(my_list)
+        i = 0
+        for item in addlist:
+            i += item
+            return (i)
+        return (0)
