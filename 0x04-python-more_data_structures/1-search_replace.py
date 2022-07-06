edf91d64ea0074c3f0a0cbd54375343d55fe01d3
SMMaina <mainamuthoni4814@gmail.com>
2022-07-06 03:11:18 -0700

    search

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        new_list = my_list.copy()
+        new_list[:] = [x if x != search else replace for x in new_list]
+        return(new_list)
+    return (my_list)
