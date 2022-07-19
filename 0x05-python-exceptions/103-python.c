f66a1aa9aeea80594e3dcf57d2cca6cd51658b8e
SMMaina <mainamuthoni4814@gmail.com>
2022-07-19 08:50:30 +0000

    more

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,9 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - prints basic info about python lists
+
