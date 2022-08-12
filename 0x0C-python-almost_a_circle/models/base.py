f1311f6df07ba1c7c0cb7493991032972716ff90
SMMaina <mainamuthoni4814@gmail.com>
2022-08-12 04:57:05 +0000

    convert to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,13 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return json.dumps([])
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes json rep of list_objs to a file """
+        if list_objs is None or list_objes == []:
+            li = []
+        else:
+            li = [n.to_dictionary() for n in list_objs]
+        with open(cls.__name__ + ".json", "w") as file:
+            file.write(cls.to_json_string(li))
