22af646e376f7ddcfe5a930eebc7f43b65054201
SMMaina <mainamuthoni4814@gmail.com>
2022-08-12 06:27:00 +0000

    json to csv

---
+++ 0x0C-python-almost_a_circle/100-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 100-main """
+from models.rectangle import Rectangle
+from models.square import Square
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(12, 3, 4, 5)
+    r2 = Rectangle(2, 4)
+    list_rectangles_input = [r1, r2]
+
+    Rectangle.save_to_file_csv(list_rectangles_input)
+
+    list_rectangles_output = Rectangle.load_from_file_csv()
+
+
+    for rect in list_rectangles_input:
+        print("[{}] {}".format(id(rect), rect))
+
+    for rect in list_rectangles_output:
+        print("[{}] {}".format(id(rect), rect))
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -73,3 +73,46 @@ class Base:
             inst.append(cls.create(**li[t]))
 
         return inst
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ deserializes in CSV """
+        try:
+            f = open(str(cls.__name__) + ".csv")
+            f.close()
+        except:
+            return []
+        square = ['id', 'size', 'x', 'y']
+        rect = ['id', 'width', 'height', 'x', 'y']
+        if cls.__name__ == "Rectangle":
+            fieldname = rect
+        else:
+            fieldname = square
+
+        instance = []
+        with open(str(cls.__name__) + ".csv", "r") as fn:
+            cd = csv.reader(fn, delimiter=',')
+            for n, dictr in enumerate(cd):
+                if n > 0:
+                    ins = cls(1, 1)
+                    for x, y in enumerate(dictr):
+                        if y:
+                            setattr(ins, fieldname[x], int(y))
+                    instance.append(ins)
+        return instance
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ writes the json rep of list_objs """
+        if list_objs is None or list_objs == []:
+            li = []
+        with open(cls.__name__ + ".csv", "w") as fn:
+            list_objs = [i.to_dictionary() for i in list_objs]
+            rect = ['id', 'width', 'height', 'x', 'y']
+            square = ['id', 'size', 'x', 'y']
+            if cls.__name__ == "Rectangle":
+                f_csv = csv.DictWriter(fn, fieldnames=rect)
+            else:
+                f_csv = csv.DictWriter(fn, fieldnames=square)
+            f_csv.writeheader()
+            f_csv.writerows(list_objs)
